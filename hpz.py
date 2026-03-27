15782447bc64134a7584e4ed7fe03e3e4a160613
edezie <edezie07@gmail.com>
2026-03-27 15:17:36 +0800

    Update hpz.py

--- hpz.py
+++ hpz.py
@@ -79,7 +79,7 @@ class User(db.Model):
     last_seen = db.Column(db.DateTime, default=lambda: datetime.now(timezone.utc))
     created_at = db.Column(db.DateTime, default=lambda: datetime.now(timezone.utc))
     def set_password(self, password):
-        self.password_hash = generate_password_hash(password)
+     self.password_hash = generate_password_hash(password, method='pbkdf2:sha256', salt_length=8)
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
     def to_dict(self):
